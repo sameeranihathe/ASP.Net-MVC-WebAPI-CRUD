a2c9d524c2f778cb4e9ba73137efa4f22f127233
sameeranihathe <sameeranihathe@gmail.com>
2018-03-31 15:05:41 +0530

    jquery validations added

--- WebAPI/MVC/Models/mvcEmployeeModel.cs
+++ WebAPI/MVC/Models/mvcEmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace MVC.Models
     public class mvcEmployeeModel
     {
         public int EmployeeID { get; set; }
+        [Required(ErrorMessage ="This field is required")]
         public string Name { get; set; }
         public string Position { get; set; }
         public Nullable<int> Age { get; set; }
